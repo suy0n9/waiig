79aa1d8837900b584a1368132a4e96fb1a837faf
suy0n9 <suy01v9@gmail.com>
2020-03-26 23:23:00 +0900

    WIP 2.4 add parser

---
+++ parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"github.com/suy0n9/waiig/ast"
+	"github.com/suy0n9/waiig/lexer"
+	"github.com/suy0n9/waiig/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{1: 1}
+
+	// Read two tokens, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
